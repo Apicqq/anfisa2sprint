5685838c564979425046ae63c411b3db1be42102
Nikita Smykov <apic@yandex.ru>
2023-06-09 09:21:30 +0300

    Add functionality based on SQL queries

--- anfisa_for_friends/core/models.py
+++ anfisa_for_friends/core/models.py
@@ -3,7 +3,16 @@ from django.db import models
 
 class PublishedModel(models.Model):
     """Абстрактная модель. Добвляет флаг is_published."""
-    is_published = models.BooleanField(default=True)
+    is_published = models.BooleanField(default=True,
+                                       verbose_name='Опубликовано')
+
+    class Meta:
+        abstract = True
+
+
+class IsOnMainModel(models.Model):
+    is_on_main = models.BooleanField(default=False,
+                                     verbose_name='На главную')
 
     class Meta:
         abstract = True
--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from .models import Category, IceCream, Topping, Wrapper
 
-# Register your models here.
+admin.site.empty_value_display = 'Не задано'
+
+
+class IceCreamInLine(admin.TabularInline):
+    model = IceCream
+    extra = 0
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (
+        IceCreamInLine,
+    )
+    list_display = (
+        'title',
+    )
+
+
+@admin.register(IceCream)
+class IceCreamAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )
+    search_fields = ('title',)
+    list_filter = ('category',)
+    list_display_links = ('title',)
+    filter_horizontal = ('toppings',)
+
+
+admin.site.register(Topping)
+admin.site.register(Wrapper)
